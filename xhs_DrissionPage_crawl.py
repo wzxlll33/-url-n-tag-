f39ca8be567ad6b927f3943e20c1f895d475c633
wzxlll33 <wzx20041108@gmail.com>
2024-11-17 23:28:21 +0800

    Add files via upload

---
+++ xhs_DrissionPage_crawl.py
@@ -0,0 +1,151 @@
+from DrissionPage import ChromiumPage
+from bs4 import BeautifulSoup
+import time
+import csv
+
+
+tag_lst = []
+name_lst = []
+count_likes = []
+count_stars = []
+count_comments = []
+
+
+def read_file_to_list(filename):
+        with open(filename, 'r', encoding='utf-8') as file:
+            # 使用列表推导式读取每一行
+            content_list = [line.strip() for line in file]
+        return content_list
+
+
+urls = read_file_to_list('xhs_urls.txt')
+
+page = ChromiumPage()
+
+def get_data(url):
+    page.get(url)
+    flag = 0
+        # time.sleep(10)\
+    for j in range(1,11):
+        # 判断有无多出来的一栏 通过信息正确点入单个视频
+            #                /html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/div/div/div/button[1]
+        if page.s_ele('xpath:/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/div/div/div/button[1]') != None:
+            ele = page.ele(f'xpath:/html/body/div[1]/div[1]/div[2]/div[2]/div/div[4]/section[{j}]/div/div/a/span',1,1)
+        else:               
+            ele = page.ele(f'xpath:/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/section[{j}]/div/div/a/span',1,1)
+        if ele == None:
+            flag = 1
+            continue
+
+        # 点击笔记名字进入笔记详情页 并且记录此时点击元素的text 这个text即为我们需要的笔记名字
+        name_lst.append(ele.text)
+        page.actions.click(ele)
+
+        # sleep保证加载完成
+        time.sleep(1)
+        soup = BeautifulSoup(page.html,"html.parser")                         
+        
+        # 调用get_likes方法获取点赞量
+        count_likes.append(get_likes(soup))
+        
+        # 调用get_stars方法获取收藏量
+        get_stars()
+
+        # 调用get_comments方法获取评论量
+        get_comments()
+
+        # 调用get_tag方法获取所有tag
+        get_tag(soup)
+        
+        # 将flag重设 保证下一次爬取正常运行
+        flag = 0
+
+        # 将页面倒回上一层 保证下一次点击正常进行
+        page.back(1)
+
+    if flag == 1:
+         # 判断有无多出来的一栏 通过信息正确点入单个视频
+        if page.s_ele('xpath:/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/div/div/div[2]/button[1]') != None:
+            ele = page.ele(f'xpath:/html/body/div[1]/div[1]/div[2]/div[2]/div/div[4]/section[11]/div/div/a/span',1,1)
+        else:               
+            ele = page.ele(f'xpath:/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/section[11]/div/div/a/span',1,1)
+
+        # 点击笔记名字进入笔记详情页 并且记录此时点击元素的text 这个text即为我们需要的笔记名字
+        name_lst.append(ele.text)
+        page.actions.click(ele)
+
+        # sleep保证加载完成
+        time.sleep(1)
+        soup = BeautifulSoup(page.html,"html.parser")                         
+        
+        # 调用get_likes方法获取点赞量
+        count_likes.append(get_likes(soup))
+        
+        # 调用get_stars方法获取收藏量
+        get_stars()
+
+        # 调用get_tag方法获取所有tag
+        get_tag(soup)
+        
+
+    # print(count_likes)
+    # print(name_lst)
+    # print(count_comments)
+    # print(count_stars)
+    # print(tag_lst)
+
+
+def get_likes(soup):
+    likes = soup.select('#noteContainer > div.interaction-container > div.interactions.engage-bar > div > div > div.input-box > div.interact-container > div > div.left > span.like-wrapper.like-active > span.count')
+    return likes[0].get_text()
+
+
+def get_stars():
+    ele_stars = page.s_ele('xpath:/html/body/div[5]/div[1]/div[4]/div[3]/div/div/div[1]/div[2]/div/div[1]/span[2]/span')
+    if ele_stars.text == '收藏':
+        stars_temp = 0
+    else:
+        stars_temp = ele_stars.text
+    count_stars.append(stars_temp)
+         
+
+def get_comments():
+    ele_comments = page.s_ele('xpath:/html/body/div[5]/div[1]/div[4]/div[3]/div/div/div[1]/div[2]/div/div[1]/span[3]/span')
+    if ele_comments.text == '评论':
+        comment_temp = 0
+    else:
+        comment_temp = ele_comments.text
+    count_comments.append(comment_temp)
+
+
+def get_tag(soup):
+    lst_temp = []
+    temp_tag = soup.findAll("a",{"class":"tag"})
+    for tag in temp_tag:
+        lst_temp.append(tag.get_text())
+    tag_lst.append(lst_temp)
+    
+
+def __main__():
+    for i in range(len(urls)):
+        print(f'正在爬取{urls[i]}')
+        get_data(urls[i])
+        print("done")
+    filename = 'xhs_data.csv'
+
+    # 使用'write'模式打开文件
+    with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+        # 创建一个csv的写入器
+        writer = csv.writer(csvfile)
+        
+        # 写入标题行（可选）
+        writer.writerow(['Tag', 'Name', 'Likes', 'Stars', 'Comments'])
+        
+        # 遍历列表并写入数据
+        for i in range(len(name_lst)):
+            writer.writerow([tag_lst[i], name_lst[i], count_likes[i], count_stars[i], count_comments[i]])
+
+    print(f'数据已写入 {filename}')
+
+
+__main__()
